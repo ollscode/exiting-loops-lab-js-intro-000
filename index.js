c8acfb87b6cc748bdf061f9d68d97c291bef22a6
Oscar <mundonatural@gmail.com>
2020-02-24 13:08:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+
+function breakOut(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] == stopValue) {
+      break;
+    }
+
+    array[i] = changeValue;
+  }
+
+  return array;
+}
